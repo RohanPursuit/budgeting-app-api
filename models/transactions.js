97e3be13e1b403fa85af80994c2964b0307804cb
Rohan-Sterling <rohansterling@pursuit.org>
2022-01-22 14:44:21 -0500

    add array literal into object literal

--- models/transactions.js
+++ models/transactions.js
@@ -1,72 +1,80 @@
-export default [
-    {
-      date: "April 1",
-      name: "",
-      source: "Income",
-      location: "",
-      amount: 1000,
-    },
-    {
-      date: "April 1",
-      name: "",
-      source: "Taxes",
-      location: "",
-      amount: -300,
-    },
-    {
-      date: "April 1",
-      name: "",
-      source: "Retirement",
-      location: "",
-      amount: -200,
-    },
-    {
-      date: "April 1",
-      name: "",
-      source: "Savings",
-      location: "",
-      amount: -100,
-    },
-    {
-      date: "April 1",
-      name: "",
-      source: "Credit Card Payment",
-      location: "",
-      amount: -100,
-    },
-    {
-      date: "April 5",
-      name: "",
-      source: "Monthly Birthday Money from Aunt Tilda",
-      location: "",
-      amount: 20,
-    },
-    {
-      date: "April 5",
-      name: "",
-      source: "Coffee",
-      location: "",
-      amount: -4,
-    },
-    {
-      date: "April 5",
-      name: "",
-      source: "Internet",
-      location: "",
-      amount: -100,
-    },
-    {
-      date: "April 3",
-      name: "",
-      source: "Groceries",
-      location: "",
-      amount: -76,
-    },
-    {
-      date: "April 3",
-      name: "",
-      source: "Pet Food",
-      location: "",
-      amount: -7,
-    },
-  ];
+export default {
+  "john": 
+  {
+    "userName": "john",
+    "password": "password",
+    "items":
+    [
+      {
+        date: "April 1",
+        name: "",
+        source: "Income",
+        location: "",
+        amount: 1000,
+      },
+      {
+        date: "April 1",
+        name: "",
+        source: "Taxes",
+        location: "",
+        amount: -300,
+      },
+      {
+        date: "April 1",
+        name: "",
+        source: "Retirement",
+        location: "",
+        amount: -200,
+      },
+      {
+        date: "April 1",
+        name: "",
+        source: "Savings",
+        location: "",
+        amount: -100,
+      },
+      {
+        date: "April 1",
+        name: "",
+        source: "Credit Card Payment",
+        location: "",
+        amount: -100,
+      },
+      {
+        date: "April 5",
+        name: "",
+        source: "Monthly Birthday Money from Aunt Tilda",
+        location: "",
+        amount: 20,
+      },
+      {
+        date: "April 5",
+        name: "",
+        source: "Coffee",
+        location: "",
+        amount: -4,
+      },
+      {
+        date: "April 5",
+        name: "",
+        source: "Internet",
+        location: "",
+        amount: -100,
+      },
+      {
+        date: "April 3",
+        name: "",
+        source: "Groceries",
+        location: "",
+        amount: -76,
+      },
+      {
+        date: "April 3",
+        name: "",
+        source: "Pet Food",
+        location: "",
+        amount: -7,
+      },
+    ]
+  }
+};
